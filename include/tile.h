00c25486c14754717d4f3816b191a1a4ce54fcb7
Luna <lunyyuwu@gmail.com>
2025-01-28 13:22:36 +0100

    flag got flaggot

--- include/tile.h
+++ include/tile.h
@@ -13,9 +13,32 @@ public:
 
     void restart();
 
+    void changeTile(int value);
+
+    void openTile();
+
+    void flagTile();
+
 private:
+
+    bool m_isOpened = false;
+    bool m_isFlagged = false;
+    int m_value = 0; // 0, 1, 2, 3, 4, 5, 6, 7, 8 are numbers, -1 is bomb, 9 is unknown and 10 is flag
+
     sf::RectangleShape m_tile;
-    sf::Texture m_tileTexture;
+
+    sf::Texture m_tileUnkwnownTexture;
+    sf::Texture m_tile0Texture;
+    sf::Texture m_tile1Texture;
+    sf::Texture m_tile2Texture;
+    sf::Texture m_tile3Texture;
+    sf::Texture m_tile4Texture;
+    sf::Texture m_tile5Texture;
+    sf::Texture m_tile6Texture;
+    sf::Texture m_tile7Texture;
+    sf::Texture m_tile8Texture;
+    sf::Texture m_tileFlagTexture;
+    sf::Texture m_tileMineTexture;
 
 
 };
--- src/tile.cpp
+++ src/tile.cpp
@@ -2,7 +2,14 @@
 
 void Tile::update(sf::Int32 deltaMS)
 {
-
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && !m_isFlagged)
+    {
+        this->openTile();
+    }
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::X) && !m_isOpened)
+    {
+        this->flagTile();
+    }
 }
 
 void Tile::render(sf::RenderWindow& window)
@@ -13,12 +20,113 @@ void Tile::render(sf::RenderWindow& window)
 void Tile::init()
 {
     m_tile.setSize(sf::Vector2f(32.f, 32.f));
-    m_tileTexture.loadFromFile("../sprites/Tiles/TileUnknown.png");
-    m_tile.setTexture(&m_tileTexture);
-    m_tile.setPosition(sf::Vector2f(896.f, 752.f));
+    m_tileUnkwnownTexture.loadFromFile("../sprites/Tiles/TileUnknown.png");
+    m_tile.setTexture(&m_tileUnkwnownTexture);
+    m_tile.setPosition(sf::Vector2f(974.f, 524.f));
+
+    m_tile0Texture.loadFromFile("../sprites/Tiles/Tile0.png");
+    m_tile1Texture.loadFromFile("../sprites/Tiles/Tile1.png");
+    m_tile2Texture.loadFromFile("../sprites/Tiles/Tile2.png");
+    m_tile3Texture.loadFromFile("../sprites/Tiles/Tile3.png");
+    m_tile4Texture.loadFromFile("../sprites/Tiles/Tile4.png");
+    m_tile5Texture.loadFromFile("../sprites/Tiles/Tile5.png");
+    m_tile6Texture.loadFromFile("../sprites/Tiles/Tile6.png");
+    m_tile7Texture.loadFromFile("../sprites/Tiles/Tile7.png");
+    m_tile8Texture.loadFromFile("../sprites/Tiles/Tile8.png");
+    m_tileFlagTexture.loadFromFile("../sprites/Tiles/TileFlag.png");
+    m_tileMineTexture.loadFromFile("../sprites/Tiles/TileMine.png");
+
 }
 
 void Tile::restart()
 {
 
+}
+
+void Tile::changeTile(int value)
+{
+    switch (value)
+    {
+        case 1:
+        {
+            m_tile.setTexture(&m_tile1Texture);
+            break;
+        }
+        case 2:
+        {
+            m_tile.setTexture(&m_tile2Texture);
+            break;
+        }
+        case 3:
+        {
+            m_tile.setTexture(&m_tile3Texture);
+            break;
+        }
+        case 4:
+        {
+            m_tile.setTexture(&m_tile4Texture);
+            break;
+        }
+        case 5:
+        {
+            m_tile.setTexture(&m_tile5Texture);
+            break;
+        }
+        case 6:
+        {
+            m_tile.setTexture(&m_tile6Texture);
+            break;
+        }
+        case 7:
+        {
+            m_tile.setTexture(&m_tile7Texture);
+            break;
+        }
+        case 8:
+        {
+            m_tile.setTexture(&m_tile8Texture);
+            break;
+        }
+        case -1:
+        {
+            m_tile.setTexture(&m_tileMineTexture);
+            break;
+        }
+        case 9:
+        {
+            m_tile.setTexture(&m_tileUnkwnownTexture);
+            break;
+        }
+        case 10:
+        {
+            m_tile.setTexture(&m_tileFlagTexture);
+            break;
+        }
+		default:
+	    {
+            m_tile.setTexture(&m_tile0Texture);
+            break;
+	    }
+    }
+	
+}
+
+void Tile::openTile()
+{
+    this->changeTile(m_value);
+    m_isOpened = true;
+}
+
+void Tile::flagTile()
+{
+    if (!m_isFlagged)
+    {
+        this->changeTile(10);
+    }
+    else
+    {
+        this->changeTile(9);
+    }
+    m_isFlagged = !m_isFlagged;
+    
 }
