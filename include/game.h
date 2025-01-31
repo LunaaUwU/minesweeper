3f2bbfdede880b63e3546e4fa06b6c882b5875c1
Luna <132057024+LunaaUwU@users.noreply.github.com>
2025-01-31 23:25:57 +0100

    spawn positions

--- include/game.h
+++ include/game.h
@@ -17,8 +17,18 @@ public:
     void restart();
 
 private:
-    Tileset* tileset = new Tileset();
 
+    Tileset* m_tileset = new Tileset();
 
+    int m_rows = 0;
+    int m_columns = 0;
+
+    int m_firstSpawnPosX = 0; // terminar esto
+    int m_firstSpawnPosY = 0;
+
+    int m_spawnPosX = 0;
+    int m_spawnPosY = 0;
+
+    int m_rowNumber = 0;
 
 };
--- src/game.cpp
+++ src/game.cpp
@@ -3,7 +3,7 @@
 
 void Game::update(const sf::Int32 deltaMS)
 {
-    tileset->update(deltaMS);
+    m_tileset->update(deltaMS);
 }
 
 void Game::render(sf::RenderWindow& window)
@@ -11,7 +11,7 @@ void Game::render(sf::RenderWindow& window)
     
     window.clear();
 
-    tileset->render(window);
+    m_tileset->render(window);
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
     {
@@ -23,15 +23,47 @@ void Game::render(sf::RenderWindow& window)
 
 void Game::init()
 {
-    tileset->init(2, 2);
+    m_rows = 2;
+    m_columns = 2;
 
-    tileset->activate(928, 508);
-    tileset->activate(960, 508);
-    tileset->activate(928, 540);
-    tileset->activate(960, 540);
+    m_tileset->init(m_rows, m_columns);
+
+    // 1920/2 = 960
+	if (m_rows % 2 == 0)
+	{
+        // 960-32 = 928
+        m_spawnPosX = 928 - m_rows * 32;
+	}
+    else
+    {
+        // 960-16 = 944
+        m_spawnPosX = 944 - m_rows * 32;
+    }
+
+    // 1080/2 = 540
+    if (m_columns % 2 == 0)
+    {
+	    // 540-32 = 508
+        m_spawnPosY = 508 - m_columns * 32;
+    }
+    else
+    {
+        // 540-16 = 524
+        m_spawnPosY = 524 - m_columns * 32;
+    }
+    
+    for (int i = 0; i < m_rows * m_columns; i++)
+    {
+        m_tileset->activate(m_spawnPosX, m_spawnPosY);
+        m_spawnPosX += 32; // terminar esto tambien
+    }
+    
+    m_tileset->activate(960, 508);
+    m_tileset->activate(928, 540);
+    m_tileset->activate(960, 540);
 }
 
 void Game::restart()
 {
-    tileset->restart();
+    m_tileset->restart();
 }
